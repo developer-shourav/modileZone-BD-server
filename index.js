41b9cc37ff8cf0e912c7a650c2364d3263c81e1e
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-29 12:17:36 +0600

    server updated

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const express = require('express');
-const bodyParser = require('body-parser') ;
-const cors = require('cors') ;
-require('dotenv').config();
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectId;
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5q9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 8000;
 const app = express()
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5q9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+
+
+// Middleware
 const corsConfig = {
     origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     }
-app.use(cors(corsConfig))
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+  app.use(cors(corsConfig))
+  app.use(express.json())
 
 
+async function run() {
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Sir ! Welcome to modibleZoon BD ')
-})
-
-
-client.connect(err => {
-const productsCollection = client.db("modileZoneBd").collection("productsData");
-const buyingCollection = client.db("modileZoneBd").collection("buyingData");
-
-    //-------------- Add phones-------------------------
+    try {
+        const productsCollection = client.db("modileZoneBd").collection("productsData");
+        const buyingCollection = client.db("modileZoneBd").collection("buyingData");
+        
+        //-------------- Add phones-------------------------
 
     app.post("/addProducts", async (req, res) => {
 
@@ -104,13 +104,22 @@ const buyingCollection = client.db("modileZoneBd").collection("buyingData");
     })
 
 
+        // Send a ping to confirm a successful connection
+       await client.db("admin").command({ ping: 1 });
+       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
 
+    finally {
+        // Ensures that the client will close when you finish/error
+        /* await client.close(); */
+      }
 
+}
 
-
-
-});
-  
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello Sir ! Welcome to modibleZoon BD ')
+})
 
 app.listen(port, () => {
     console.log(`Running On the port:${port}`)
